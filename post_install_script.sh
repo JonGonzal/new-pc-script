0f14ade5afc2ca5b9dc75ca032b82ae13a5a1e50
jongonzal <12769872+JonGonzal@users.noreply.github.com>
2024-05-23 13:25:12 -0500

    Organized files

--- post_install_script.sh
+++ post_install_script.sh
@@ -13,8 +13,8 @@ sudo apt install fuse
 sudo snap install --classic code
 sudo apt install autorandr
 
+mkdir -p /mnt/dev
 mkdir -p ~/documents/repos/
-
 
 # Update Node , used by some LSP's.
 sudo npm cache clean -f
@@ -31,40 +31,26 @@ fi
 # Check's if display exists and install GUI apps 
 if [ -n "${DISPLAY+x}" ]; then 
 	sudo apt install i3 -y
-
 	sudo apt install gimp -y
-
-
 
 	# Installing latest chrome
 	wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-
 	sudo apt install ./google-chrome-stable_current_amd64.deb -y
-
 	rm google-chrome-stable_current_amd64.deb
-
 fi
 
 # Installing latest nvim and allow for global access of the command "nvim"
 # Also adds nvim dependencies 
-
 wget -P ~/ https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
-
 sudo chmod u+x ~/nvim.appimage
+sudo mv -f ~/nvim.appimage /usr/bin/nvim
+sudo chmod 755 /usr/bin/nvim
 
-mv ~/nvim.appimage ~/usr/bin/nvim
-sudo chmod 755 ~/usr/bin/nvim
-
-# Kickstarting nvim 
-# Creates the NVIM environment
-git clone https://github.com/JonGonzal/kickstart.nvim.git ~/documents/repos/kickstart_nvim
-
-sudo mv ~/documents/repos/kickstart_nvim/ ~/.config/
-mv ~/.config/kickstart_nvim/ ~/.config/nvim
+# Kickstarting nvim, creates the NVIM environment
+sudo cp -fr ~/new-pc-script/kickstart_nvim ~/.config/nvim
 
 # Installs personal configs 
-git clone https://github.com/JonGonzal/new-pc-script.git ~/documents/repos/new-pc-script/
+sudo cp -f ~/new-pc-script/i3_config ~/.config/i3/config
+sudo cp ~/new-pc-script/.bashrc ~/
+sudo cp ~/new-pc-script/.tmux.conf ~/
 
-sudo cp -f ~/documents/repos/new-pc-script/i3_config ~/.config/i3/config
-sudo cp ~/documents/repos/new-pc-script/.bashrc ~/
-sudo cp ~/documents/repos/new-pc-script/.tmux.config ~/
