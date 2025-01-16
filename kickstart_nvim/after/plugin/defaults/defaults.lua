d2b3bc21311ca002402b16a95f8e53468f9bf438
jon <jon@jon.com>
2025-01-16 17:24:47 -0600

    Added lsp's to install

--- kickstart_nvim/after/plugin/defaults/defaults.lua
+++
@@ -1,7 +0,0 @@
-vim.opt.relativenumber = true
-
-vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
-
-vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
-
-vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
