2cede7b8c7ba0b0c33084eb5978e90273d0f0296
jongonzal <12769872+JonGonzal@users.noreply.github.com>
2025-06-20 15:56:36 -0500

    misc, alternate init.lua config

--- nvim/init.lua
+++ nvim/init.lua
@@ -464,15 +464,30 @@ mason_lspconfig.setup {
   ensure_installed = vim.tbl_keys(servers),
 }
 
-mason_lspconfig.setup_handlers {
-  function(server_name)
-    require('lspconfig')[server_name].setup {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      settings = servers[server_name],
-    }
-  end,
+require('mason-lspconfig').setup {
+  ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
+  automatic_installation = false,
+  handlers = {
+    function(server_name)
+      local server = servers[server_name] or {}
+      -- This handles overriding only values explicitly passed
+      -- by the server configuration above. Useful when disabling
+      -- certain features of an LSP (for example, turning off formatting for ts_ls)
+      server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+      require('lspconfig')[server_name].setup(server)
+    end,
+  },
 }
+
+ -- mason_lspconfig.setup_handlers {
+ --   function(server_name)
+ --     require('lspconfig')[server_name].setup {
+ --       capabilities = capabilities,
+ --       on_attach = on_attach,
+ --       settings = servers[server_name],
+ --     }
+ --   end,
+ -- }
 
 local lspconfig = require('lspconfig')
 local configs = require('lspconfig/configs')
